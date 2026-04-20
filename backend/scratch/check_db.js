8e4e325a818f81cbd7f08aca0ac4a3a6c0c50986
Rashini Savindya <rashinijayasekarajp@gmail.com>
2026-04-20 17:40:32 +0530

    chore: remove temporary debug and scratch files

--- backend/scratch/check_db.js
+++
@@ -1,5 +0,0 @@
-const db = require('../db');
-db.query("DESCRIBE places", (err, res) => {
-    if(!err) console.log("places columns:", res.map(r => r.Field));
-    process.exit(0);
-});
--- backend/scratch/list_tables.js
+++
@@ -1,9 +0,0 @@
-const db = require("../db");
-db.query("SHOW TABLES", (err, results) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.table(results);
-  process.exit(0);
-});
